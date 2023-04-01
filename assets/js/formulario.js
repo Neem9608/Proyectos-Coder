bdc9415acbe0ef219306d18f3e0435574f97c661
admer escobar <admerescobar@gmail.com>
2023-04-01 18:32:21 -0300

    pude solucionar con la ayuda de Luciano Tutor por exelencia, una dos condiciones que tenia que agregar a la variable edad.

--- assets/js/formulario.js
+++ assets/js/formulario.js
@@ -9,24 +9,32 @@ nombre = prompt("cual es tu nombre?");
 }
 let edad = parseInt(prompt("que edad tienes?"));
 console.log("edad del solicitante: ", edad);
+while (edad == vacio || isNaN(edad)){
+  edad = parseInt(prompt("ingrese una edad valida"));
+}
 if (edad >= 25) {
-  // ejecutando el codigo solo si la condicion es verdadera
 } else {
-  // solo si la condicion es falsa
-  alert("no eres legible para adoptar a chan");
-}
+  alert("comprate un peluche");
 
-let telefono = parseInt(prompt("ingrese su numero de telefono"));
+  }
+
+
+  let telefono = parseInt(prompt("ingrese su numero de telefono"));
 console.log("telefono del solicitante: ", telefono);
-let vivienda = prompt("usted vive en un cautry? responda: 'si o no' ");
-console.log("el solicitante vive en un coutry?: ", vivienda);
-if (vivienda == "si" || vivienda == "Si" || vivienda == "SI"){
-  // ejecutando el codigo solo si la condicion es verdadera
-  alert("nos estaremos contactando en breve");
-}
-if (vivienda == "no"|| vivienda == "No" || vivienda == "NO") {
-  alert("no eres legible para adoptar a chan");
-}
 
+while (telefono == vacio) {
+  alert ("por favor, complete este espacio");
+  telefono = parseInt(prompt("ingrese su numero de telefono"));  }
+
+// let vivienda = prompt("usted vive en un cauntry? responda: 'si o no' ");
+// console.log("el solicitante vive en un coutry?: ", vivienda);
+// if (vivienda == "si" || vivienda == "Si" || vivienda == "SI"){
+//   // ejecutando el codigo solo si la condicion es verdadera
+//   alert("nos estaremos contactando en breve");
+// }
+// if (vivienda == "no"|| vivienda == "No" || vivienda == "NO") {
+//   alert("no eres legible para adoptar a chan");
+// }
+// .........................
 // let telefono =  parseInt( prompt ("ingrese su numerod e telefono"));
 // console.log (telefono);
