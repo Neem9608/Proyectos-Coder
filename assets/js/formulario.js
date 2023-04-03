f6738d1c8abe4afc6fa6b922e7abc1dc30ab8ae2
admer escobar <admerescobar@gmail.com>
2023-04-03 19:29:35 -0300

    se agrego alguno datos, comente el codigo y agregue el readme.

--- assets/js/formulario.js
+++ assets/js/formulario.js
@@ -1,40 +1,61 @@
-// interacccion con el solicitante
-let vacio = "";
+
+// funcion para calcular vacio
+function validarVacio(valor) {
+  let vacio = "";
+  // condicion para validar vacio
+  if (valor == vacio) {
+    return true;
+  } else return false;
+}
+// declaracion de Variable nombre
 let nombre = prompt("cual es tu nombre?");
 console.log("nombre del solicitante: ", nombre);
-
-while (nombre == vacio) {
-alert ("por favor, complete este espacio");
-nombre = prompt("cual es tu nombre?");
+// ciclo para validar vacio de la variable 'nombre'.
+while (validarVacio(nombre)) {
+  alert("por favor, complete este espacio");
+  nombre = prompt("cual es tu nombre?");
+  console.log("nombre del solicitante: ", nombre);
 }
+// declaracion de Variable edad
 let edad = parseInt(prompt("que edad tienes?"));
 console.log("edad del solicitante: ", edad);
-while (edad == vacio || isNaN(edad)){
+// ciclo para validar vacio de la variable 'edad'.
+while (validarVacio(edad) || isNaN(edad)) {
   edad = parseInt(prompt("ingrese una edad valida"));
+  console.log("edad del solicitante: ", edad);
 }
+// condicion para validar si la edad cumple con los requeriminetos de edad minima para adoptar al canino.
 if (edad >= 25) {
-} else {
-  alert("comprate un peluche");
-
-  }
-
-
   let telefono = parseInt(prompt("ingrese su numero de telefono"));
-console.log("telefono del solicitante: ", telefono);
-
-while (telefono == vacio) {
-  alert ("por favor, complete este espacio");
-  telefono = parseInt(prompt("ingrese su numero de telefono"));  }
-
-// let vivienda = prompt("usted vive en un cauntry? responda: 'si o no' ");
-// console.log("el solicitante vive en un coutry?: ", vivienda);
-// if (vivienda == "si" || vivienda == "Si" || vivienda == "SI"){
-//   // ejecutando el codigo solo si la condicion es verdadera
-//   alert("nos estaremos contactando en breve");
-// }
-// if (vivienda == "no"|| vivienda == "No" || vivienda == "NO") {
-//   alert("no eres legible para adoptar a chan");
-// }
-// .........................
-// let telefono =  parseInt( prompt ("ingrese su numerod e telefono"));
-// console.log (telefono);
+  console.log("telefono del solicitante: ", telefono);
+  while (validarVacio(telefono) || isNaN(telefono)) {
+    telefono = parseInt(prompt("ingrese un telefono valido"));
+    console.log("telefono del solicitante: ", telefono);
+  }
+  // declaracion de Variable vivienda
+  let vivienda = prompt("¿usted vive en un countries? responda: 'si o no' ");
+  console.log("el solicitante vive en un coutry?: ", vivienda);
+  // ciclo para validar vacio de variable 'vivienda'.
+  while (validarVacio(vivienda)) {
+    alert("por favor, complete este espacio");
+    vivienda = prompt("¿usted vive en un cauntry? responda: 'si o no' ");
+    console.log("el solicitante vive en un coutry?: ", vivienda);
+  }
+  // condicion para validar si el interesado en adoptar el canino vive en un countries, el interesado tiene que elegir 'SI o NO'.
+  if (vivienda == "si" || vivienda == "Si" || vivienda == "SI") {
+    alert("nos estaremos contactando en breve");
+    alert("por favor, recargue la pagina con F5 para elegir otras opciones ");
+  }
+  if (vivienda == "no" || vivienda == "No" || vivienda == "NO") {
+    alert(
+      "No cumples la condicion de edad para adoptar a Shan,Estamos buscando personas que vivan en un countries. "
+    );
+    alert("por favor, recargue la pagina con F5 para elegir otras opciones ");
+  }
+  // condicion para validar si el interesado en adoptar el canino NO vive en un countries.
+} else {
+  alert(
+    "No cumples la condicion de edad para adoptar a Shan,Estamos buscando personas mayores a 25 años"
+  );
+  alert("por favor, recargue la pagina con F5 para elegir otras opciones ");
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+Validacion de datos para adoptar un canino
+
+El código proporcionado implementa funciones, variables y condiciones.
+El codigo esta diseñado para validarVacio() de edad(), nombre(), telefono() y vivienda ().
+Hay condiciones como la edad() minima para adoptar el canino, tener o vivir en vivienda() en un countries.
+Si no cumple las condiciones les mostrara algunas alertas para que puedan seguir probando el codigo.
+
+Feedback
+
+Espero su devolucion con todo el optimismo posible, si hay algo o muchas cosas que mejorar y me van a ayudar en los proximos proyectos, siempre son totalmente bienvenidos y recibidos con total aprecio.
+
+P/D, A tener en cuenta: ---> Este proyecto es solo una simulacion y es solo para esta preEntrega, la proxima preEnterga sera algo quiza muy distinto a lo que se muestra en esta ocasion.
+
+Nelson Escobar (Alumno en Coderhouse - Curso de JavaScript)
